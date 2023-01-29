7777fad361f632172ddd8ddb438b81c559d94f31
Jin Park <jinpk@users.noreply.github.com>
2023-01-29 16:47:11 +0900

    fix: Fix vocabs build errors

--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -3,15 +3,19 @@ import { IsNotEmpty } from 'class-validator';
 import { LoginDto } from './login.dto';
 
 export class SignUpDto extends PickType(LoginDto, ['password']) {
-  @ApiProperty({ required: true, description: '닉네임' })
+  @ApiProperty({ description: '닉네임' })
   @IsNotEmpty()
   nickname: string;
 
-  @ApiProperty({ required: true, description: '이름' })
+  @ApiProperty({ description: '이름' })
   @IsNotEmpty()
   name: string;
 
-  @ApiProperty({ required: true, description: '인증 ID (코드인증까지 완료)' })
+  @ApiProperty({ description: '인증 ID (코드인증까지 완료)' })
   @IsNotEmpty()
   verifiId: string;
+
+  @ApiProperty({ description: '국가 2자리 코드' })
+  @IsNotEmpty()
+  countryCode: string;
 }
--- src/vocabs/vocabs.controller.ts
+++ src/vocabs/vocabs.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get, Post, Param, Body, UseInterceptors } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { VocabDto } from './dto/vocab.dto';
 
 import {
-    ApiBearerAuth,
-    ApiOkResponse,
-    ApiOperation,
-    ApiTags,
-} from '@nestjs/swagger'
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+} from '@nestjs/swagger';
 import { VocabsService } from './vocabs.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 
@@ -14,42 +22,42 @@ import { FileInterceptor } from '@nestjs/platform-express';
 @ApiTags('vocabs')
 @ApiBearerAuth()
 export class VocabsController {
-    constructor(private readonly vocabsService: VocabsService) {}
+  constructor(private readonly vocabsService: VocabsService) {}
 
-    // @Get('cats/all')
-    // async findAll(): string {
-    //   return 'This action returns all cats';
-    // }
-  
-    // @Get(':id')
-    // findOne(@Param('id') id: number): string {
-    //     console.log(id)
-    //   return 'This action returns one cats';
-    // }
-  
-    @Get(':id')
-    @ApiOperation({
-      summary: 'Vocab 상세 조회',
-    })
-    @ApiOkResponse({
-      type: VocabDto,
-    })
-    async getVocab(@Param('vocab_id') vocab_id: string) {
-      const vocab = new VocabDto();
-      return vocab;
-    }
+  // @Get('cats/all')
+  // async findAll(): string {
+  //   return 'This action returns all cats';
+  // }
 
-    @Get('listvocab')
-    async getListVocab() {
-      return 'This action returns some list vocab';
-    }
+  // @Get(':id')
+  // findOne(@Param('id') id: number): string {
+  //     console.log(id)
+  //   return 'This action returns one cats';
+  // }
 
-    @Post('fileupload')
-    @ApiOperation({
-      summary: 'Vocab Excel File 업로드',
-    })
-    @UseInterceptors(FileInterceptor('excel_file', 1, ))
-    fileUpload(@Body('excel_data') excel_data: FormData) {
-      return 'This action POST vocabs by reading excel data';
-    }
+  @Get(':id')
+  @ApiOperation({
+    summary: 'Vocab 상세 조회',
+  })
+  @ApiOkResponse({
+    type: VocabDto,
+  })
+  async getVocab(@Param('vocab_id') vocab_id: string) {
+    const vocab = new VocabDto();
+    return vocab;
+  }
+
+  @Get('listvocab')
+  async getListVocab() {
+    return 'This action returns some list vocab';
+  }
+
+  @Post('fileupload')
+  @ApiOperation({
+    summary: 'Vocab Excel File 업로드',
+  })
+  @UseInterceptors(FileInterceptor('excel_file'))
+  fileUpload(@UploadedFile('excel_file') excel_file) {
+    return 'This action POST vocabs by reading excel data';
+  }
 }
