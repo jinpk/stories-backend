e669570d3534791a4dea1702336da50d48e3ffa1
Jin Park <jinpk@no-reply.github.com>
2023-02-05 11:30:49 +0900

    style: admin module

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { UsersModule } from 'src/users/users.module';
 import { AdminService } from './admin.service';
 import { Admin, AdminSchema } from './schemas/admin.schema';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Admin.name, schema: AdminSchema }]),
-    UsersModule,
   ],
   providers: [AdminService],
   exports: [AdminService],
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,14 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { UsersService } from 'src/users/users.service';
 import { Admin, AdminDocument } from './schemas/admin.schema';
 
 @Injectable()
 export class AdminService {
   constructor(
     @InjectModel(Admin.name) private adminModel: Model<AdminDocument>,
-    private usersService: UsersService,
   ) {}
 
   async validateAdmin(username: string, password: string) {
