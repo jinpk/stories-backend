a2fdbd98940436acd354e5645f9548773569897d
Jin Park <jinpk@no-reply.github.com>
2023-02-15 12:08:40 +0900

    refactor: Refactor email module

--- src/common/providers/index.ts
+++ src/common/providers/index.ts
@@ -1,2 +1,3 @@
 export * from './utils.service';
 export * from './excel.service';
+export * from './email.service';
--- src/event/event.module.ts
+++ src/event/event.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { EmailModule } from 'src/email/email.module';
 import { NotificationsModule } from 'src/notifications/notifications.module';
 import { EventService } from './event.service';
 
 @Module({
-  imports: [NotificationsModule,EmailModule],
+  imports: [NotificationsModule],
   providers: [EventService],
 })
 export class EventModule {}
