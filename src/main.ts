1480c881a143520468b33fbc90158ba5ab8a00b6
Jin Park <jinpk@no-reply.github.com>
2023-02-15 10:37:02 +0900

    refactor: Files

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { join } from 'path';
+import { APPLICATION_PORT } from './app.constant';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,9 +11,13 @@ async function bootstrap() {
 
   app.enableCors({ origin: '*' });
   app.useGlobalPipes(new ValidationPipe());
+
+  //swagger
   const config = new DocumentBuilder()
     .setTitle('Stories API')
-    .setDescription('The stories API description\n모든 리스트 조회에서 excel: 1 요청시 response에 excel buffer로 내려감.')
+    .setDescription(
+      'The stories API description\n모든 리스트 조회에서 excel: 1 요청시 response에 excel buffer로 내려감.',
+    )
     .setVersion('0.1')
     .addTag('auth', '서비스 인증')
     .addTag('users', '회원 관리')
@@ -22,15 +27,14 @@ async function bootstrap() {
     .addTag('files', '공통 파일 API')
     .addBearerAuth()
     .build();
-
   const document = SwaggerModule.createDocument(app, config);
-  
   SwaggerModule.setup('openapi', app, document);
 
+  //ejs
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.setViewEngine('ejs');
 
-  await app.listen(3000);
+  await app.listen(APPLICATION_PORT);
 }
 bootstrap();
