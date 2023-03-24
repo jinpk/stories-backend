fd29da10abe563b802ab99364d1c4b08e9c8e65d
jinhorsad <jinho39858@rsad.co.kr>
2023-03-24 15:16:28 +0900

    refactor: auth constant

--- src/auth/auth.constant.ts
+++ src/auth/auth.constant.ts
@@ -5,7 +5,6 @@ export const TTMIK_API_PATH_VERIFY =
   '/wp-json/TTMIKRestAuth/v1/force-authorization-email';
 export const TTMIK_API_PATH_RESET_PASSWORD =
   '/wp-json/TTMIKRestAuth/v1/force-reset-password';
-
 export const TTMIK_API_PATH_VALIDATE_PASSWORD =
   '/wp-json/TTMIKRestAuth/v1/validate-current-password';
 export const VERIFY_EMAIL_CHECK_PATH = '/auth/email/verification';
