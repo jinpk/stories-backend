b3fd8f850258ff4d53bae0e8b302202da1b8a11b
jinhorsad <jinho39858@rsad.co.kr>
2023-05-25 11:31:02 +0900

    fix: audioplayer 리스트 조회 수정

--- src/audioplayer/dto/audioplayer.dto.ts
+++ src/audioplayer/dto/audioplayer.dto.ts
@@ -2,6 +2,9 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class AudioPlayerDto{
     id?: string;
+
+    @ApiProperty({})
+    contentsId: string;
   
     @ApiProperty({})
     contentsSerialNum: string;
